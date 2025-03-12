f48b40f00b0db4e9b6ac7c80fa482a261c3f8070
opiliper <kapituliruy@gmail.com>
2025-03-12 14:26:48 +0700

    CreateUserDTO changed CreateUserDTO changed

--- Share/Users/DTOs/CreateUserDTO.cs
+++ Share/Users/DTOs/CreateUserDTO.cs
@@ -1,9 +1,23 @@
 namespace Users.DTOs;
 
-public record CreateUserDTO
-(
-  string? Name,
-  string Email,
-  string Password,
-  byte[]? Image
-);
+public class CreateUserDTO
+{
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string Password { get; set; }
+  public byte[]? Image { get; set; }
+  public CreateUserDTO (
+    string name,
+    string email,
+    string password,
+    byte[]? image
+  )
+  {
+    Name = name;
+    Email = email;
+    Password = password;
+    Image = image;
+  }
+
+  public CreateUserDTO() {}
+}
